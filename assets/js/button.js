5c38de370a1f59bb3f5874b46c293cf675be52b1
DeepBhatiya <deepbhatiya256@gmail.com>
2024-09-09 10:32:04 +0530

    commit changes

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,14 +1,10 @@
-$('.btn').stop().on('click', function(){
-	button = $(this);
-	button.addClass('clicked');
-	
-	setTimeout(function(){
-		button.addClass('success');
-	},300);
-	
-	
-	setTimeout(function(){
-		button.removeClass('clicked');
-		button.removeClass('success');
-	},4000);
-});
+$ = function(id) {
+	return document.getElementById(id);
+  }
+  
+  var show = function(id) {
+	  $(id).style.display ='block';
+  }
+  var hide = function(id) {
+	  $(id).style.display ='none';
+  }
